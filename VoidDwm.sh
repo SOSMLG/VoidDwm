2b2bd0258a53841417304aa77b8439485141c90f
SOSMLG <117748270+SOSMLG@users.noreply.github.com>
2025-02-27 17:43:03 +0100

    VoidDwm.sh

--- VoidDwm.sh
+++ VoidDwm.sh
@@ -1,5 +1,6 @@
 #Important package
 sudo xbps-install base-devel freetype-devel libXft-devel libXinerama-devel libX11-devel libxcb-devel harfbuzz-devel xorg-server xbacklight xbindkeys xvkbd xorg elogind dbus xinput gcc flameshot make binutils compton git NetworkManager network-manager-applet thunar-archive-plugin thunar-volman file-roller lxappearance dialog mtools avahi acpi acpid gvfs xfce4-power-manager pulseaudio pavucontrol pulsemixer feh papirus-icon-theme exa maim dunst xdotool unzip micro geany geany-plugins redshift firefox-esr vlc font-awesome terminus-font libnotify lightdm 
+sudo xbps-install -S void-repo-multilib void-repo-multilib-nonfree
 #extracting
 tar -xf Voidsuckless.tar.gz 
 #Void suckless location
