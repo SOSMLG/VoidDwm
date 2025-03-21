31425658c566bf3b6ba369164352531018746f83
SOSMLG <117748270+SOSMLG@users.noreply.github.com>
2025-03-21 08:57:37 +0100

    autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,6 @@
-exec compton &
 exec pulseaudio &
 exec feh --bg-fill ~/wallpaper.jpg &
 exec "setxkbmap -layout us,ar" &
 exec "setxkbmap -option 'grp:alt_shift_toggle'" &
-exec dwm &
-exec slstuatus 
+exec compton &
+exec dwm 
